2723ee6814a32b79c517523dc2235b5142323f04
Gustavo <gstonin95@gmail.com>
2020-07-08 19:54:51 +0100

    added event listeners for changing tabs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 import { loadPage } from '../scripts/functionLoadPage'
 import { homeContent } from '../scripts/functionHomeContent'
+import { menuContent } from '../scripts/functionMenuContent'
+import { contactContent } from '../scripts/functionContactContent'
 
 loadPage()
 homeContent()
+
+const navBarTabs = document.querySelectorAll('.navBarElement');
+const contentDiv = document.querySelector('#content');
+
+navBarTabs.forEach((tab) => {tab.addEventListener('click', (e) => {
+
+    navBarTabs.forEach((tab) => tab.classList.remove('activeTab'));
+    e.target.classList.add('activeTab');
+
+    contentDiv.removeChild(contentDiv.lastChild);
+
+    switch (e.target.id) {
+        case 'home':
+            homeContent()
+        break;
+    
+        case 'menu':
+            menuContent()
+        break;
+
+        case 'contact':
+            contactContent()
+        break;
+    }
+
+    });
+});
+
+    // const activeNavBar = document.querySelector('#home');
+    // activeNavBar.classList.add('activeTab');
